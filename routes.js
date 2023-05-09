f7325fbc9986c2e4e2ad5bd7d843e1779edefeea
malepsyche <yew.benji@gmail.com>
2023-05-09 16:09:25 +0800

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,7 +1,10 @@
 var express = require("express");       // import express
 var router = express.Router();          // import router class from express
+
+// routing for GET requests
 router.get("/", function(req, res){     // router.get = route handling for when someone sends a get request for your browser URL
           console.log("Someone just sent a get request for your URL when they went onto your webpage");
+          res.render("home-page");          // renders the HTML file home-page.ejs as a response (res) to the client's web browser
 });
 
 module.exports = router;
