795a44964351a303d8afb4d76e669795a8d699db
malepsyche <yew.benji@gmail.com>
2023-05-09 15:44:01 +0800

    Update Files

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
+// import frameworks and modules
 var express = require("express");       // import express
 var path = require("path");          // import path
+var routes = require("./routes");       // import routes (local file)
 
+
+// configure app.js
 var app = express();          // app = express application
 app.set("port", process.env.PORT || 3000);        // set port number to PORT if not NULL, else 3000
+app.use(routes);    // use routes.ejs
+app.set("views", path.join(__dirname, "views"));  // set the built-in property "views" to the local path __dirname/views (__dirname is the current directory)
+app.set("view engine", "ejs")
 
-//__________________________________________________________________
-// Starting the server
 
 // app is a variable representing the Express application.
 // listen is a function that starts the server and tells it to listen for incoming requests on a specified port number.
