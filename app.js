8b8516f93d1a59697495f2c9be90a9e68355f163
malepsyche <yew.benji@gmail.com>
2023-05-10 18:06:31 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
-// import frameworks and modules
-var express = require("express");       // import express
-var path = require("path");          // import path
-var routes = require("./routes");       // import routes (local file)
+// Configurations for main application file app.js
+const express = require("express");       // import express
+const path = require("path");          // import path
+const app = express();          // app = express application
+
+app.set("views", path.join(__dirname, "views"));  // set the built-in property "views" to the local path __dirname/views (__dirname is the current directory) -> This tells the application that all our ejs template (view) files are in the directory "__dirname/views" so that we won't have to specify the absolute path for every page that we render 
+app.set("view engine", "ejs");          // set our view engine to ejs (embedded javascript)
 
 
-// configure app.js
-var app = express();          // app = express application
+// Import local files
+const homePage = require("./routes/home-page");
+
+
+// Direct the URLs to the corresponding local files
+app.use("/", homePage);
+
+
+// Start the server
 app.set("port", process.env.PORT || 3000);        // set port number to PORT if not NULL, else 3000
-app.use(routes);    // use routes.ejs
-app.set("views", path.join(__dirname, "views"));  // set the built-in property "views" to the local path __dirname/views (__dirname is the current directory)
-app.set("view engine", "ejs")
-
-
-// app is a variable representing the Express application.
-// listen is a function that starts the server and tells it to listen for incoming requests on a specified port number.
-// app.get("port") is a function that retrieves the value of the port property that was previously set using the app.set method.
-// The first argument passed to listen is the port number that the server should listen on. This value is retrieved by calling app.get("port").
-// The second argument passed to listen is a callback function that is called when the server starts listening for incoming requests. It logs a message to the console indicating that the server has started and specifies the port number that it's listening to
 app.listen(app.get("port"), function(){
-          console.log("Server started on port " + app.get("port"));
+          console.log("Server started on port " + app.get("port"));   // starts the server and tells it to listen for incoming requests on a specified port number.
 });
 
+ 
