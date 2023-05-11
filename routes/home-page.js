95b4b0c10a5f187f63092c5f065a170e438969ce
malepsyche <yew.benji@gmail.com>
2023-05-11 12:53:40 +0800

    Update home-page.js

--- routes/home-page.js
+++ routes/home-page.js
@@ -7,9 +7,5 @@ router.get("/", function(req, res){     // router.get = route handling for when 
           res.render("home-page");          // renders the HTML file home-page.ejs as a response (res) to the client's web browser
 }); 
 
-// router.get("/about", function(req, res){     // router.get = route handling for when someone sends a get request for your browser URL
-//           console.log("Someone just sent a get request for your URL when they went onto your webpage");
-//           res.render("home-page");          // renders the HTML file home-page.ejs as a response (res) to the client's web browser
-// }); 
 
 module.exports = router;
