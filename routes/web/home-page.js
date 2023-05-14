d204b71d40867f70024387d7ae969a03dbb966df
malepsyche <yew.benji@gmail.com>
2023-05-14 18:51:12 +0800

    Change home-page.js file path

--- routes/web/home-page.js
+++ routes/web/home-page.js
@@ -4,7 +4,7 @@ const router = express.Router();          // import router class from express
 // routing for GET requests
 router.get("/", function(req, res){     // router.get = route handling for when someone sends a get request for your browser URL
           console.log("Someone just sent a get request for your URL when they went onto your webpage");
-          res.render("home-page");          // renders the HTML file home-page.ejs as a response (res) to the client's web browser
+          res.render("home-page/home-page");          // renders the HTML file home-page.ejs as a response (res) to the client's web browser
 }); 
 
 
